b94f991f72428b64ca4cef894483f71f7e41cb19
NVD919 <49793442+NVD919@users.noreply.github.com>
2019-06-09 18:12:46 -0500

    Update Tennessee Eastman Supervised.py

--- Tennessee Eastman Supervised.py
+++ Tennessee Eastman Supervised.py
@@ -1,20 +1,11 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue May 14 22:44:05 2019
 
 @author: Robot Hands (github.com/nvd919)
 """
-# =============================================================================
-# The first hurdle to clear is getting all of the data from the 44 files loaded
-# into the model. glob.glob() will handle all files fitting a common naming
-# convention, but will return them in no particular order. Thus, we use 
-# sorted() to counter that. This is of critical importance because the training
-# data must correspond to the accompanied test data, otherwise the results
-# will probably be totally useless; like studying Spanish for a Math exam.
-# =============================================================================
-
 import pandas as pd
 import os
+from sklearn.model_selection import RepeatedKFold as rkf
 
 DATA_FOLDER = r"E:\ML Projects\Tennessee Eastman\TE_process_dataset"
 NUM_FILES = 22
@@ -24,3 +15,10 @@ test_files = [os.path.join(DATA_FOLDER, "d{:0>2}_te.dat".format(i)) for i in ran
 
 training_data = [pd.read_csv(f, sep=' ', index_col = None) for f in training_files]
 test_data = [pd.read_csv(f, sep=' ') for f in test_files]
+
+# =============================================================================
+#   Due to the relatively small training data (490 samples per fault) I think 
+#   a K-fold is necessary per fault.
+# =============================================================================
+seed = 10
+rkf = rkf(n_splits = 5, n_repeats = 10, random_state = seed)
